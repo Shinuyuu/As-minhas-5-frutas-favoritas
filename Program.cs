e9542130d8c7e87b3944d1640b6136018f660612
thur04kalinauskas@gmail.com <e107ti@etec.sp;gov;br>
2023-03-03 22:13:38 -0300

    PEGAS AS 5 FRUTAS FAVORITAS, ainda to triste pela musica

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿
+Console.WriteLine("__________As 5 frutas que eu mais gosto____________");
+
+Console.WriteLine("Tomate NC: Solanum lycopersicum");
+
+Console.WriteLine("pêssego NC: Prunus persica ");
+
+Console.WriteLine("Maçã NC: Malus domestica Borkh ");
+
+Console.WriteLine("Uva NC:  Vitis vinífera ");
+
+Console.WriteLine("Abacaxi NC: Ananas comosus ");
+
+Console.WriteLine("=================================");
